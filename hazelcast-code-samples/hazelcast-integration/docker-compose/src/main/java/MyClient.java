bf50022579580e9e79fa0f2085998ed4dd1eecec
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-08-02 07:59:06 -0500

    committing changes

--- hazelcast-code-samples/hazelcast-integration/docker-compose/src/main/java/MyClient.java
+++ hazelcast-code-samples/hazelcast-integration/docker-compose/src/main/java/MyClient.java
@@ -1,5 +1,8 @@
+import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IQueue;
 
 /**
  * 
@@ -8,7 +11,17 @@ import com.hazelcast.client.config.XmlClientConfigBuilder;
  */
 public class MyClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
 		ClientConfig clientConfig = new XmlClientConfigBuilder().build();
+		final HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+		
+		System.out.println(clientConfig.toString());
+		
+		IQueue<String> queue = client.getQueue("queue");
+		queue.put("Hello!");
+		System.out.println("Message sent by Hazelcast Client!");
+		
+		HazelcastClient.shutdownAll();
 	}
 }
