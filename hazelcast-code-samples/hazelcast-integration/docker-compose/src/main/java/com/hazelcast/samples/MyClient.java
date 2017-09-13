d372a82ca678ecbb992851ef0109beeeb3170219
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-09-13 08:15:15 -0500

    configuring local cache

--- hazelcast-code-samples/hazelcast-integration/docker-compose/src/main/java/com/hazelcast/samples/MyClient.java
+++ hazelcast-code-samples/hazelcast-integration/docker-compose/src/main/java/com/hazelcast/samples/MyClient.java
@@ -1,4 +1,6 @@
 package com.hazelcast.samples;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IQueue;
 
@@ -16,6 +19,8 @@ import com.hazelcast.core.IQueue;
  */
 @SpringBootApplication
 public class MyClient implements CommandLineRunner {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyClient.class);
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(MyClient.class, args);
@@ -28,6 +33,14 @@ public class MyClient implements CommandLineRunner {
 		final HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
 		
 		System.out.println(clientConfig.toString());
+		
+		// ************************ config embeded instance *****************************
+		logger.debug("**************************** STARTING HZ EMBEDED INSTANCE *****************");
+		HazelcastInstance newHazelcastInstance = Hazelcast.newHazelcastInstance();
+		logger.debug("HZ EMBEDED INSTANCE STARTED ...");
+		
+		logger.debug("**************************** END STARTING HZ EMBEDED INSTANCE *****************");
+		
 		
 		IQueue<String> queue = client.getQueue("queue");
 		queue.put("Hello!");
